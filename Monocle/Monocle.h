4261a273f9d42491306d57aed73312cd81e29b80
Quentin RIBIERRE <quentin@prolificinteractive.com>
2015-12-23 18:01:37 -0500

    Initial Commit

---
+++ Monocle/Monocle.h
@@ -0,0 +1,19 @@
+//
+//  Monocle.h
+//  Monocle
+//
+//  Created by Quentin Ribierre on 12/23/15.
+//  Copyright © 2015 Prolific Interactive Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Monocle.
+FOUNDATION_EXPORT double MonocleVersionNumber;
+
+//! Project version string for Monocle.
+FOUNDATION_EXPORT const unsigned char MonocleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Monocle/PublicHeader.h>
+
+
